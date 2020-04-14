8ee08c717b972b4abf253edb375e7f541083c565
Sergey Shevtsov <dev.serzh@gmail.com>
2020-04-14 22:27:59 +0300

    instruments and categories tests

--- app/bands/tests.py
+++ app/bands/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
-from bands.models import City
+from bands.models import (
+    City, InstrumentCategory, Instrument,
+)
 
 
 class TestBands(TestCase):
@@ -11,3 +13,21 @@ class TestBands(TestCase):
 
         assert City.objects.count() == 1
         assert city.name == 'Moscow'
+
+    @staticmethod
+    def test_instruments_and_categories_creation():
+        # create new category
+        category = InstrumentCategory.objects.create(name='strings')
+
+        # add new instrument related to category
+        guitar = Instrument(name='guitar', category=category)
+        bass = Instrument(name='bass guitar', category=category)
+        drums = Instrument(name='drums')
+        Instrument.objects.bulk_create([guitar, bass, drums])
+
+        assert category.instruments.count() == 2
+        assert Instrument.objects.count() == 3
+        assert category.instruments.first().name == 'guitar'
+        assert category.instruments.last().name == 'bass guitar'
+        assert guitar.category.name == 'strings'
+        assert drums.category is None
